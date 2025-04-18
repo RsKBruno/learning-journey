75e9c2485ba06ed82ee4e9e66202fd3aeca387ce
Bruno Alves <107444663+RsKBruno@users.noreply.github.com>
2025-04-18 11:38:46 -0300

    update link

--- leetcode-solutions/9. Palindrome Number.py
+++ leetcode-solutions/9. Palindrome Number.py
@@ -1,5 +1,6 @@
 # 9. Palindrome Number
 # Given an integer x, return true if x is a palindrome, and false otherwise.
+# Link: https://leetcode.com/problems/palindrome-number/description/
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
