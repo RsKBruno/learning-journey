47acc21e159fd90f54c7a29e2b8238f871e8c39e
Bruno Alves <107444663+RsKBruno@users.noreply.github.com>
2025-04-18 22:02:02 -0300

    initial commit

---
+++ sql-leetcode-solutions/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,18 @@
+/*
+| product_id | low_fats | recyclable |
+| ---------- | -------- | ---------- |
+| 0          | Y        | N          |
+| 1          | Y        | Y          |
+| 2          | N        | Y          |
+| 3          | Y        | Y          |
+| 4          | N        | N          |
+
+Write a solution to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+link: https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+*/
+SELECT product_ID
+FROM Products
+WHERE low_fats = 'Y' and recyclable = 'Y'
